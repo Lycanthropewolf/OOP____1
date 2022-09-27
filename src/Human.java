c5b0c891b17d31001f82566ca8a40bbbdc5c58bd
Антон <antonolegovich2017@icloud.com>
2022-09-27 22:24:13 +0600

    OOP 1 domashka

---
+++ src/Human.java
@@ -0,0 +1,21 @@
+public class Human {
+    String name;
+    String city;
+    int dateOfBirth;
+    String jobTitle;
+
+    Human(String name, String city, int dateOfBirth, String jobTitle) {
+        this.name = name;
+        this.city = city;
+        this.dateOfBirth = dateOfBirth;
+        this.jobTitle = jobTitle;
+
+    }
+
+    public String toString() {
+        return "Привет! меня зовут " + name + " я из города - " + city + " я родился в " + dateOfBirth + " году. Я работаю на должности"
+                + jobTitle + ". Будем знакомы.";
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Human human1 = new Human("Максим", " Минск", 2022 - 35, " бренд - менеджер");
+        Human human2 = new Human("Аня", "Москва", 2022 - 29," методист образовательных программ");
+        Human human3 = new Human("Катя", "Калининград", 2022 - 28, " продакт-менеджер");
+        Human human4 = new Human("Артем", "Москва", 2022 - 27," директор по развитию бизнеса");
+
+        System.out.println(human1.toString());
+        System.out.println(human2.toString());
+        System.out.println(human3.toString());
+        System.out.println(human4.toString());
     }
 }
